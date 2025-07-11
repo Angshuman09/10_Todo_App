b28a52340a95f609e338141f9dc133ca4040de5f
Angshuman <angshumankashyap69yap69@gmail.com>
2025-07-11 15:30:59 +0530

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { TodoProvider } from './contexts/TodoContext'
+import TodoForm from './components/TodoForm'
+import TodoItem from './components/TodoItem'
 
 function App() {
   const [todos, setTodos] = useState([])
@@ -18,8 +20,8 @@ function App() {
   }
 
   const toggleComplete=(id)=>{
-    setTodos((prev)=>prev.map((todo)=>
-      prevTodo.id===id?{...prevTodo,completed: !todo.completed}:todo
+    setTodos((prev)=>prev.map((prevTodo)=>
+      prevTodo.id===id?{...prevTodo,completed: !prevTodo.completed}:prevTodo
     ))
   }
 
@@ -43,9 +45,17 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
           <div className="mb-4">
             {/* Todo form goes here */}
+            <TodoForm/>
           </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            {todos.map((todo)=>(
+              <div key={todo.id}
+              className='w-full'
+              >
+                <TodoItem todo={todo}/>
+              </div>
+            ))}
           </div>
         </div>
       </div>
